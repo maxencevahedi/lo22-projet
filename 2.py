f020f3f0efbc29769da0009c045c151a34b74d62
Maxence <maxence.vahedi@proton.me>
2026-03-12 15:22:57 +0100

    Jupyter notebook v1

---
+++ 2.py
@@ -0,0 +1,45 @@
+from fiabilipym import Component, System
+
+s = System()
+
+# Components
+
+hba1 = Component("HBA1", 1.25 * 10**-6, 7e-3)
+hba2 = Component("HBA2", 1.25 * 10**-6, 7e-3)
+linka = Component("LINKA", 2.52 * 10**-6, 3e-3)
+linkb = Component("LINKB", 2.52 * 10**-6, 3e-3)
+hub1 = Component("HUB1", 1.71 * 10**-6, 2e-3)
+hub2 = Component("HUB2", 1.71 * 10**-6, 2e-3)
+linkc = Component("LINKC", 2.52 * 10**-6, 3e-3)
+linkd = Component("LINKD", 1.71 * 10**-6, 3e-3)
+diska = Component("DISKA", 10**-5, 5e-4)
+diskb = Component("DISKB", 10**-5, 5e-4)
+
+# System
+s["E"] = [hba1, hba2]
+
+s[hba1] = linka
+s[hba2] = linkb
+
+s[linka] = hub1
+s[linkb] = hub2
+
+s[hub1] = linkc
+s[hub2] = linkd
+
+s[linkc] = diska
+s[linkd] = diskb
+
+s[diska] = s[diskb] = "S"
+
+import pylab as pl
+
+timerange = range(0, 2 * 365 * 24, 1)
+reliability = [s.reliability(t) for t in timerange]
+availability = [s.availability(t) for t in timerange]
+
+
+pl.plot(timerange, availability)
+pl.plot(timerange, reliability)
+pl.title("2")
+pl.show()
